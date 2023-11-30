17dd76c55b94d17c52fa0561efd3057e9021905e
Lisa <145290867+lisawo1@users.noreply.github.com>
2023-11-30 17:09:06 +0300

    Update 2.1.py

--- 2.1.py
+++ 2.1.py
@@ -1,21 +1,19 @@
 def maxk(arr, k):
-    n = len(arr)
-    max_sum = float('-inf')  
-    for i in range(n - k + 1):
-        current_sum = sum(arr[i:i+k])  
+    max_sum = float('-inf')
+    if k<len(arr):
+        for i in range(len(arr) - k + 1):
+            current_sum = sum(arr[i:i+k])  
 
         if current_sum > max_sum:
             max_sum = current_sum
+        return max_sum
+    
 
-    return max_sum
-
-
-
-size = int(input("введите длину массива: "))
 arr = list(map(int, input("введите элементы массива: ").split()))
 k = int(input("введите число последовательных элементов: "))
 
-
-result = maxk(arr, k)
-print("макс сумма", k, "последовательных элементов:", result)
-
+if maxk==True:
+    result = maxk(arr, k)
+    print("макс сумма", k, "последовательных элементов:", result)
+else:
+    print("введите другие значения")
