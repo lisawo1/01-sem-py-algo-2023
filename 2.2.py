130f010a9211d72b2f7343210c738e307f992832
Lisa <145290867+lisawo1@users.noreply.github.com>
2023-11-30 17:43:03 +0300

    Update 2.2.py

--- 2.2.py
+++ 2.2.py
@@ -1,22 +1,22 @@
-def sieveOfEratosthenes(limit):
-    isPrime = [True] * (limit + 1)
-    primes = []
-
-    for i in range(2, int(limit ** 0.5) + 1):
-        if isPrime[i]:
-            for j in range(i * i, limit + 1, i):
-                isPrime[j] = False
-
-    for i in range(2, limit + 1):
-        if isPrime[i]:
-            primes.append(i)
-
+def sieveOfEratosthenes(x):
+    primes=[]
+    for i in range (x+1):
+        primes.append(i)
+    primes[1] = 0
+    i = 2
+    while i <= x:
+        if primes[i] != 0:
+            j = i + i
+            while j <= x:
+                primes[j] = 0
+                j = j + i
+        i += 1
+    primes = [i for i in primes if i != 0]
     return primes
 
 
 def divisor(x):
     primes = sieveOfEratosthenes(x)
-
     gr_divisor = 1
     for prime in primes:
         if x % prime == 0:
@@ -28,3 +28,5 @@ def divisor(x):
 x = int(input("Введите число: "))
 gr_divisor = divisor(x)
 print(f"наибольший простой делитель - {gr_divisor}")
+
+
